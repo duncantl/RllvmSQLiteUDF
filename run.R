60f1b9b5f8066aa2e1e5b161f5d9a0e785b92b1a
Duncan Temple Lang <dtemplelang@ucdavis.edu>
2017-06-20 10:58:40 -0700

    crashes on Linux with llvm routine, but not with C compiled code

--- run.R
+++ run.R
@@ -1,20 +1,27 @@
     # git@github.com:duncantl/RSQLiteUDF.git
 
 # RSQLiteUDF needs RSQlite version 1.0-0, not greater than this, for now!
-if(require("RSQLite") && require("RSQLiteUDF") && require("Rllvm")) {
+library("RSQLite")
+library("RSQLiteUDF")
+library("Rllvm") 
 
-m = parseIR("fib_basic.ll")
+
+m = parseIR("fib.ll")
+
+llvmAddSymbol(getNativeSymbolInfo("sqlite3_value_int"))
+llvmAddSymbol(getNativeSymbolInfo("sqlite3_result_int"))
+
 ee = ExecutionEngine(m)
 
 # Check code works
-ans = .llvm(m$fib1, 10L, .ee = ee)
+ans = .llvm(m$fib2, 10L, .ee = ee)
 print(ans)
 # Answer = 55
 
 db = dbConnect(SQLite(), "foo")
 sqliteExtension(db) 
 
-ptr = getPointerToFunction(m$fib1, ee)
+ptr = getPointerToFunction(m$sqlFib3, ee)
 createSQLFunction(db, ptr@ref, "fib", nargs = 1L)
 d = dbGetQuery(db, "SELECT fib(x) FROM mytable LIMIT 5")
 
