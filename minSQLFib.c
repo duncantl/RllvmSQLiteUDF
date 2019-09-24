badb50e269711225984aff96bbf902e2d0044e14
Duncan Temple Lang <duncan@r-project.org>
2019-09-24 06:49:24 -0700

    error message handling

--- minSQLFib.c
+++ minSQLFib.c
@@ -38,6 +38,24 @@ sqlDexp(sqlite3_context *context, int argc, sqlite3_value **argv)
 
 
 void
+dummy(sqlite3_context *context, int argc, sqlite3_value **argv)
+{
+   int x = sqlite3_value_int(argv[0]);
+   double y = sqlite3_value_double(argv[1]);
+   double z = sqlite3_value_double(argv[2]);       
+   sqlite3_result_int(context, x + y + z);
+}
+
+void
+err(sqlite3_context *context, int argc, sqlite3_value **argv)
+{
+    if(argc != 0)
+       sqlite3_result_error(context, "expecting 0 arguments for err", -1);
+    sqlite3_result_int(context, 2);
+}
+
+
+void
 R_setSQLite3API(void *ptr)
 {
    sqlite3_api = (sqlite3_api_routines*) ptr;
